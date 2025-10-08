9a36091d4cd343c5302a22eeaf830fa1d47e4022
Ahmed Heikal <aheikal424@icloud.com>
2025-10-08 05:52:20 +0200

    emitting and receiving events

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,27 +1,33 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyparser.json());
+app.use(cors());
 
 app.post("/events", (req, res) => {
   const event = req.body;
 
   // send request to all running services
-  axios.post("http://localhost:3000/events", event).catch((err) => {
-    console.log(err.message);
-  });
+
+  //PostService
   axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log(err.message);
   });
+  //CommentService
   axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  //QueryService
+  axios.post("http://localhost:4002/events", event).catch((err) => {
     console.log(err.message);
   });
 
   res.send({ status: "ok" });
 });
 
-app.listen(4004, () => {
+app.listen(4005, () => {
   console.log("event-bus listening on 4005");
 });
