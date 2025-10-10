a3ec3119ec67fb8b269234414ff11dd12ed6016c
Ahmed Heikal <aheikal424@icloud.com>
2025-10-10 21:38:38 +0200

    updating comment content

--- comments/index.js
+++ comments/index.js
@@ -39,8 +39,31 @@ app.post("/posts/:id/comments", async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("CommentService: Received Event", req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === "commentModerated") {
+    const { postId, id, status, content } = data;
+
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "commentUpdated",
+      data: {
+        id,
+        postId,
+        status,
+        content,
+      },
+    });
+  }
 
   res.send({});
 });
